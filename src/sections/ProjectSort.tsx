604e39c57218f82276a7d80d7fb18abc6af8c9db
Michael Svajhart <msvajhart@outlook.com>
2025-08-23 14:33:24 -0400

    adding basic fillout of project Sort

--- src/sections/ProjectSort.tsx
+++ src/sections/ProjectSort.tsx
@@ -1,22 +1,51 @@
-import React, {useState} from "react"
+import React, {useEffect, useState} from "react"
 import axios from "axios";
 
 interface TSortFormData {
-    array: number[];
+    data: number[];
     sortType: "bubbleSort" | "insertionSort" | "heapSort"  | "mergeSort"; //allowed sorty types
+}
+
+interface TSortResponse {
+    data: number[];
 }
 
 
 const ProjectSort: React.FC = () => {
 
     const [formData, setFormData] = useState<TSortFormData>({
-        array: [3,1,5,2],
+        data: [3,1,5,2],
         sortType: "bubbleSort"
     })
 
+    const [resultsData, setResultsData] = useState<number[]>([])
 
-    const handleSort = () => {
+    useEffect(()=>{
+        console.log(resultsData)
+    }, [resultsData])
 
+    const handleSort = async () => {
+
+        try {
+            const response = await axios.post("https://322059ov62.execute-api.us-east-1.amazonaws.com/prod/sort", formData, {
+                headers: {
+                'Content-Type': 'application/json', // Specify content type
+                },
+                timeout: 5000, // Set a timeout for the request (in milliseconds)
+            });
+            let newArr: TSortResponse = response.data
+            console.log( typeof(newArr.data));
+            setResultsData(newArr.data);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
+     const handleAddEle = () => {
+        setFormData({
+            ...formData,
+            data:[...formData.data, Math.floor(Math.random() * 10000)]
+        });
     }
 
     return (
@@ -24,9 +53,16 @@ const ProjectSort: React.FC = () => {
             <section className="section" id="projectb">
                 <div className="container">
                     <h1 className="title">Project B</h1>
+                    
+                    <button onClick={handleAddEle}>add random number</button>
                     <div>
-
+                        {formData.data.map(ele => <span>{ele},</span>)}
                     </div>
+                    <div>
+                        <button onClick={handleSort}>Trigger Sort</button>
+                    </div>
+                    {resultsData.map(ele => <span>{ele},</span>)}
+
                 </div>
             </section>
            
