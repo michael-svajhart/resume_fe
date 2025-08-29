e3ec2294e1c27ea7146cf92dc097d4103deb8f65
Michael Svajhart <msvajhart@outlook.com>
2025-08-29 15:43:16 -0400

    adding shuffle, delete elements, add more then 1 element at a time, styling elements to be blocks

--- src/layout/ProjectNav.tsx
+++ src/layout/ProjectNav.tsx
@@ -3,7 +3,7 @@ const ProjectNav: React.FC = () => {
     return(
         <aside>
             <a href="/#projectapple" className="Nav-Link">Project Apple</a>
-            <a href="/#projectb" className="Nav-Link">Project B</a>
+            <a href="/#projectsort" className="Nav-Link">Project Sort</a>
         </aside>
     );
 }
--- src/sections/ProjectSort.tsx
+++ src/sections/ProjectSort.tsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react"
 import axios from "axios";
+import { Button } from "react-bootstrap";
 
 interface TSortFormData {
     data: number[];
@@ -21,8 +22,8 @@ const ProjectSort: React.FC = () => {
     const [resultsData, setResultsData] = useState<number[]>([])
 
     useEffect(()=>{
-        console.log(resultsData)
-    }, [resultsData])
+        setResultsData([])
+    }, [formData])
 
     const handleSort = async () => {
 
@@ -41,34 +42,124 @@ const ProjectSort: React.FC = () => {
         }
     }
 
-     const handleAddEle = () => {
+     const handleAddEle = (count:number) => {
+
+        let newNumbers: number[] = []
+
+        for(var i = 0; i < count; i++){
+            newNumbers.push(Math.floor(Math.random() * 10000))
+        }
+
         setFormData({
             ...formData,
-            data:[...formData.data, Math.floor(Math.random() * 10000)]
+            data:[...formData.data, ...newNumbers]
         });
+    }
+
+    const handleShuffle = (): void => {
+        console.log("shuffle")
+        let array = formData.data;
+        let currentIndex = array.length, randomIndex;
+
+        // While there remain elements to shuffle.
+        while (currentIndex !== 0) {
+            // Pick a remaining element.
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            // And swap it with the current element.
+            [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+        }
+
+        setFormData({
+            ...formData,
+            data: array
+        })
+
+    }
+
+    const handleDelete = (index: number) => {
+        let tempArr = formData.data;
+        tempArr.splice(index, 1);
+        setFormData({
+            ...formData,
+            data: tempArr
+        })
     }
 
     return (
         <>
-            <section className="section" id="projectb">
+            <section className="section" id="projectsort">
                 <div className="container">
-                    <h1 className="title">Project B</h1>
-                    
-                    <button onClick={handleAddEle}>add random number</button>
-                    <div>
-                        {formData.data.map(ele => <span>{ele},</span>)}
+                    <h1 className="title">Project Sort</h1>
+                    <div className="mb-2">
+                        <h5 className="subtitle">Sort Types</h5>
+                        <Button className="me-1 active" onClick={()=>{setFormData({
+                            ...formData,
+                            sortType: "bubbleSort"
+                        })}}>Bubble Sort</Button>
+                        <Button className="me-1" onClick={()=>{setFormData({
+                            ...formData,
+                            sortType: "insertionSort"
+                        })}}>Insertion Sort</Button>
+                        <Button className="me-1" onClick={()=>{setFormData({
+                            ...formData,
+                            sortType: "heapSort"
+                        })}}>Heap Sort</Button>
+                        <Button className="me-1" onClick={()=>{setFormData({
+                            ...formData,
+                            sortType: "mergeSort"
+                        })}}>Merge Sort</Button>
+                    </div>
+                    <div className="mb-1">
+                        <h5 className="subtitle">Add elements to the array to be sorted</h5>
+                        <Button className="me-1" onClick={()=>handleAddEle(1)}>Add a random number</Button>
+                        <Button className="me-1" onClick={()=>handleAddEle(10)}>add 10 random numbers</Button>
+                        <Button className="me-1" onClick={()=>handleAddEle(100)}>add 100 random numbers</Button>
+                        <Button className="me-1" onClick={handleShuffle}>Shuffle</Button>
                     </div>
                     <div>
-                        <button onClick={handleSort}>Trigger Sort</button>
+                        <h5 className="subtitle">Elements in array</h5>
+                        <div className="mt-2 mb-1">
+                            {formData.data.map((ele,index) => <BubbleElement ele={ele} index={index} onClick={()=>handleDelete(index)}/>)}
+                        </div>
+                        <div>
+                            <Button onClick={handleSort}>Trigger Sort</Button>
+                        </div>
                     </div>
-                    {resultsData.map(ele => <span>{ele},</span>)}
-
+                    {
+                        resultsData.length > 0 &&
+                        <div>
+                            <h5 className="subtitle">Sorted Array</h5>
+                            {resultsData.map((ele, index) => <BubbleElement ele={ele} index={index}/>)}
+                        </div>
+                    }
                 </div>
             </section>
            
         </>
     )
 };
+
+interface TBubbleElementProps {
+    ele: number,
+    index: number,
+    onClick?: ((index: number) => void);
+}
+const BubbleElement: React.FC<TBubbleElementProps> = ({ele, index, onClick}) => {
+
+    const handleClick = () =>{
+        if(!onClick) return;
+
+        onClick(index);
+    }
+    return(
+        <div className="border border-secondary rounded d-flex d-inline-flex bg-light  p-2 me-1 mb-1" key={index} onClick={handleClick} >
+            <div className="justify-content-center">{ele}</div>
+        </div>
+    )
+}
 
 
 export default ProjectSort;
